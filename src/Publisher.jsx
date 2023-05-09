075f2620b4c1c1c9457e3367fc6c6454a37bd7df
nickWalks <69692804+nickWalks@users.noreply.github.com>
2023-05-08 18:09:27 -0700

    delete

--- src/Publisher.jsx
+++ src/Publisher.jsx
@@ -1,31 +1,38 @@
 import React, { useState } from 'react';
-import database from './firebase';
-import { ref, push, serverTimestamp } from 'firebase/database';
+import { getDatabase, ref, push, remove, onValue, set } from 'firebase/database';
 
 const Publisher = () => {
     const [message, setMessage] = useState('');
 
-    const handlePublish = () => {
-        if (message.trim() !== '') {
-            const messagesRef = ref(database, 'messages');
-            push(messagesRef, {
-                content: message,
-                timestamp: serverTimestamp(),
-            });
+    const database = getDatabase();
 
-            setMessage('');
-        }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const messagesRef = ref(database, 'messages');
+        const newMessageKey = push(messagesRef).key;
+        const newMessageRef = ref(database, `messages/${newMessageKey}`);
+        set(newMessageRef, { content: message });
+        setMessage('');
+    };
+
+    const deleteAllMessages = async () => {
+        const messagesRef = ref(database, 'messages');
+        remove(messagesRef);
     };
 
     return (
         <div className="Publisher">
-            <input
-                type="text"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                placeholder="Type your message"
-            />
-            <button onClick={handlePublish}>Publish</button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder="Type your message"
+                />
+                <button type="submit">Publish</button>
+            </form>
+            <button onClick={deleteAllMessages}>Delete All Messages</button>
         </div>
     );
 };
