ad45fc4e344ee027f6f64c2a48934d0353509312
nickWalks <69692804+nickWalks@users.noreply.github.com>
2023-05-10 09:46:00 -0700

    Update to NoticeAI

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,8 @@ export default function Header() {
         <div className="md:flex md:items-center md:justify-between">
             <div className="min-w-0 flex-1">
                 <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-                    {/*NoticeAI 👀*/}
-                    PenAI 🖊
-
+                    NoticeAI 👀
+                    {/*PenAI 🖊*/}
                 </h2>
             </div>
 
