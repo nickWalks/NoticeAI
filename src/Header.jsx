52934fba185c8a2a929b540aa2ad11bc6c14af34
nickWalks <69692804+nickWalks@users.noreply.github.com>
2023-05-09 21:32:35 -0700

    PenAI update for Graciela

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,9 @@ export default function Header() {
         <div className="md:flex md:items-center md:justify-between">
             <div className="min-w-0 flex-1">
                 <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-                    NoticeAI 👀
+                    {/*NoticeAI 👀*/}
+                    PenAI 🖊
+
                 </h2>
             </div>
 
